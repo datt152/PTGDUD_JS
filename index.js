b4e24d889bb74264f0de9b4d42dae5fd60399861
H82M19\Student <Student>
2025-01-08 10:47:38 +0700

    finish JF Part 1 Challenge 3

--- index.js
+++ index.js
@@ -10,5 +10,26 @@ if(BMIOfJohn > BMIOfMark) {
 else {
     
     console.log("Mark's BMI(" + BMIOfMark + ") is higher than John's(" + BMIOfJohn + ")");
-
 }
+function avg(array) {
+    var sum = 0;
+    for (let index = 0; index < array.length; index++) {
+        sum += array[index];
+    }
+    return sum/array.length;
+}
+var dolphinsScore = [97, 112, 101];
+var koalasScore = [109, 95, 123];
+
+if(avg(dolphinsScore) > avg(koalasScore) && avg(dolphinsScore) >= 100) {
+    console.log("Dolphins are the winner of the competition with " +avg(dolphinsScore) + " scores" );
+}
+else if(avg(koalasScore) > avg(dolphinsScore) && avg(koalasScore) >= 100){
+    console.log("Koalas are the winner of the competition with " +avg(koalasScore) + " scores" );
+}
+else if(avg(koalasScore) == avg(dolphinsScore) && avg(koalasScore) >= 100){
+    console.log("Dolphins and Koalas are draw")
+} else {
+    console.log("No one wins");
+}
+
