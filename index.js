526795bc84ae5f88504d5ae75622dfe6ae47e7ce
H82M19\Student <Student>
2025-01-08 10:24:06 +0700

    finish JF Part 1 Challenge 2

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ var massOfJohn = 92;
 var heightOfJohn = 1.95;
 var BMIOfMark = massOfMark/(heightOfMark**2);
 var BMIOfJohn = massOfJohn/(heightOfJohn**2);
-console.log("Data 1: Mark's BMI: ", BMIOfMark);
-console.log("Data 2: John's BMI: ", BMIOfJohn);
-massOfMark = 95;
-heightOfMark = 1.88;
-massOfJohn = 85;
-heightOfJohn = 1.76;
-BMIOfMark = massOfMark/(heightOfMark**2);
-BMIOfJohn = massOfJohn/(heightOfJohn**2);
-console.log("Data 1: Mark's BMI: ", BMIOfMark);
-console.log("Data 2: John's BMI: ", BMIOfJohn);
+if(BMIOfJohn > BMIOfMark) {
+    console.log("John's BMI(" + BMIOfJohn + ") is higher than Mark's(" + BMIOfMark + ")");
+}
+else {
+    
+    console.log("Mark's BMI(" + BMIOfMark + ") is higher than John's(" + BMIOfJohn + ")");
+
+}
